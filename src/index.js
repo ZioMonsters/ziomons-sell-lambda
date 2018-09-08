01ca68008d80aeefcb43a4f34a01e5c010f14032
Emanuele Caruso <ema.caruso2@gmail.com>
2018-09-08 12:50:46 +0000

    works

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 const AWS = require("aws-sdk")
+const documentClient = new AWS.DynamoDB.DocumentClient({region:'eu-west-3'});
 
 exports.handler = (event, context, callback) => {
+
+const data = JSON.parse(event.Records[0].body);
   Promise.all([
     // promise che scriva nella table cryptomon-shop-env (monsterId)
+    documentClient.put({
+        TableName: 'cryptomon-shop-staging',
+        Item: {
+            monsterId: data._id,
+            price: data._price,
+            owner: data._player
+        }
+    }).promise().catch(console.error),
     // promise che segni l'evento nella table cryptomon-events come processato
+    documentClient.put({
+        TableName: 'cryptomon-events-staging',
+        Item: {
+            transactionId: data.eventId,
+            type: 'forSale',
+            processed: true
+        }
+    }).promise().catch(console.error)
   ])
     .then(() => callback(null, event))
 }
